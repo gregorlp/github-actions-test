f844683426f3f273319e8885c0a3ae190c58424b
gregorlp <gregor613@gmail.com>
2025-07-06 17:04:40 -0300

    sixth commit

--- App.test.jsx
+++ App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import  App  from './app.jsx';
+import  App  from './src/app.jsx';
 test('muestra saludo inicial', () => {
   render(<App />);
   expect(screen.getByText(/Hola desde React CI/i)).toBeDefined();
